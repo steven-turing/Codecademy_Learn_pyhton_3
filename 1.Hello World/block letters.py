ded7ecba6701fd0b8e626e0f7e6ccd0aba0e95cf
sijw <1003909016@qq.com>
2023-12-20 19:14:03 +0800

    2022/12/20 19:13 commit

---
+++ 1.Hello World/block letters.py
@@ -0,0 +1,24 @@
+#ASCII art is a graphic design technique that uses computers for presentation and consists of pictures pieced together
+# from individual characters.
+print(" SSS  L    ")
+print("S   S L    ")
+print("S     L    ")
+print(" SSS  L    ")
+print("    S L    ")
+print("S   S L    ")
+print(" SSS  LLLLL")
+
+#My first name and last name
+print("my first name is jinwen")
+print("my last name is si")
+
+#A fun fact about yourself
+print("sing out of tune")
+
+print(" SSS   III ")
+print("S   S   I  ")
+print("S       I  ")
+print(" SSS    I  ")
+print("    S   I  ")
+print("S   S   I  ")
+print(" SSS   III ")
