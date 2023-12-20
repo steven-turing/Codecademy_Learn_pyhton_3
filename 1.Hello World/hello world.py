c7bf16fb65d5a2f0f7abeb4449c60b3b61fb4206
sijw <1003909016@qq.com>
2023-12-20 18:24:32 +0800

    Initial commit

---
+++ 1.Hello World/hello world.py
@@ -0,0 +1,4 @@
+#Welcome
+#Python is a programming language. Like other languages, it gives us a way to communicate ideas.
+#In the case of a programming language, these ideas are “commands” that people use to communicate with a computer!
+
