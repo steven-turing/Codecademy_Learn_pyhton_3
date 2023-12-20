4d4d9a9003bedd4769466faa0510c04485140990
sijw <1003909016@qq.com>
2023-12-20 19:51:43 +0800

    2022/12/20 19:51 commit

---
+++ 2.Control Flow/Control Flow.py
@@ -0,0 +1,21 @@
+#1.Introduction to Control Flow
+#This is the control flow of your program. In Python, your script will execute from the top down, until there is nothing
+# left to run. It is your job to include gateways, known as conditional statements, to tell the computer when it should
+# execute certain blocks of code. If these conditions are met, then run this function.
+
+#2.Boolean Expressions
+#In order to build control flow into our program, we want to be able to check if something is true or not.
+# A boolean expression is a statement that can either be True or False.
+
+#Example statement:My dog is the cutest dog in the world.
+example_statement = "No"
+#This is an opinion and not a boolean expression, so you would set example_statement to "No" in the editor to the right.
+# Okay, now it’s your turn:
+#Statement one:Dogs are mammals.
+statement_one = "Yes"
+#Statement two:My dog is named Pavel.
+statement_two = "Yes"
+#Statement three:Dogs make the best pets.
+statement_three ="No"
+#Statement four:Cats are female dogs.
+statement_four ="Yes"
