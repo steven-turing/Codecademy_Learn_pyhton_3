52e381ee9b83e76777049398af42990481ce8335
sijw <1003909016@qq.com>
2023-12-20 18:45:25 +0800

    2022/12/20 commit

--- 1.Hello World/hello world.py
+++ 1.Hello World/hello world.py
@@ -2,3 +2,11 @@
 #Python is a programming language. Like other languages, it gives us a way to communicate ideas.
 #In the case of a programming language, these ideas are “commands” that people use to communicate with a computer!
 
+#Change Codecademy to your name in the script to the right. Run the code to see what it does!
+#As soon as you’re ready, move on to the next exercise to begin learning to write your own Python programs!
+
+my_name = "Codecademy"
+print("Hello and welcome " + my_name + "!")
+
+my_name = "SiJinWen"
+print("Hello and Welcome " + my_name + "!")
