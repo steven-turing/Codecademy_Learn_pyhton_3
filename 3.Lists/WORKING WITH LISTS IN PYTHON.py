d0bfa0976193d0bc6dcb4eb9d70abf5ba98872ff
sijw <1003909016@qq.com>
2023-12-29 17:53:03 +0800

    2022/12/29 17:52 commit

---
+++ 3.Lists/WORKING WITH LISTS IN PYTHON.py
@@ -0,0 +1,12 @@
+# Adding by Index: Insert
+# The Python list method .insert() allows us to add an element to a specific index in a list.
+
+# The .insert() method takes in two inputs:
+# The index you want to insert into.
+# The element you want to insert at the specified index.
+front_display_list = ["Mango", "Filet Mignon", "Chocolate Milk"]
+print(front_display_list)
+
+# Your code below:
+front_display_list.insert(0,"Pineapple")
+print(front_display_list)
