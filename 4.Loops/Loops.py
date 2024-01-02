74833aa21ca5e2056e0bfdaec17af8a8ec5c0b6d
sijw <1003909016@qq.com>
2024-01-02 17:27:06 +0800

    2024/1/2 17:27 commit

--- 4.Loops/Loops.py
+++ 4.Loops/Loops.py
@@ -1,3 +1,30 @@
 # 1.
 # LoopsIn programming, this process of using an initialization, repetitions, and an ending condition is called a loop.
 # In a loop, we perform a process of iteration (repeating tasks).
+
+# 2.If we only use print(), our program might look like this:
+# Write 10 print() statements below!
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+
+# 3.For Loops: Introduction
+board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
+
+sport_games = ["football", "hockey", "baseball", "cricket"]
+
+for game in board_games:
+  print(game)
+
+
+for game in sport_games:
+  print(game)
+
+# 4.For Loops: Using Range
