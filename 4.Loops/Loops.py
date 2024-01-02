be1a983de70a8a0bbac30f7b796e49c09fed8e59
sijw <1003909016@qq.com>
2024-01-02 18:48:23 +0800

    2024/1/3 14:53 commit

--- 4.Loops/Loops.py
+++ 4.Loops/Loops.py
@@ -63,3 +63,39 @@ index = 0
 while index < length:
     print("I am learning about " + python_topics[index])
     index += 1
+
+# 7.Infinite Loops
+students_period_A = ["Alex", "Briana", "Cheri", "Daniele"]
+students_period_B = ["Dora", "Minerva", "Alexa", "Obie"]
+
+for student in students_period_A:
+    # students_period_A.append(student)
+    print(student)
+
+# 8.Loop Control: Break
+dog_breeds_available_for_adoption = ["french_bulldog", "dalmatian", "shihtzu", "poodle", "collie"]
+dog_breed_I_want = "dalmatian"
+
+for dog_breed in dog_breeds_available_for_adoption:
+    print(dog_breed)
+    if dog_breed == dog_breed_I_want:
+        print("They have the dog I want!")
+        break
+
+# 9.Loop Control: Continue
+ages = [12, 38, 34, 26, 21, 19, 67, 41, 17]
+for age in ages:
+    if age < 21:
+        continue
+    print(age)
+
+# 10.Nested Loops
+sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
+
+scoops_sold = 0
+
+for location in sales_data:
+    for data in location:
+        scoops_sold += data
+
+print(scoops_sold)
