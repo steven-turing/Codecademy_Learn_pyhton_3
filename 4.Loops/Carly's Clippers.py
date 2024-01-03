bed6b1c6b1246563cea5db0b756de34f09ac43d8
sijw <1003909016@qq.com>
2024-01-03 17:16:57 +0800

    2024/1/3 17:15 commit

--- 4.Loops/Carly's Clippers.py
+++ 4.Loops/Carly's Clippers.py
@@ -7,23 +7,23 @@ last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 total_price = 0
 
 for price in prices:
-  total_price += price
+    total_price += price
 print(total_price)
 
 # Try modifying your code to use f-string syntax for the print statement, like this:
-average_price = total_price/len(prices)
+average_price = total_price / len(prices)
 print(f"The average_price is :{average_price}")
 
 new_prices = [price - 5 for price in prices]
 print(new_prices)
 
-total_revenue  = 0
+total_revenue = 0
 
 for i in range(len(hairstyles)):
-  total_revenue += prices[i]*last_week[i]
+    total_revenue += prices[i] * last_week[i]
 print(f"The total_revenue is :{total_revenue}")
 
-average_daily_revenue = total_revenue/7
+average_daily_revenue = total_revenue / 7
 print(average_daily_revenue)
 
 # First, you need to include the index i in your loop, so you can access the corresponding hairstyle in hairstyles.
@@ -33,5 +33,5 @@ cuts_under_30 = []
 i = 0
 for i in range(len(new_prices)):
     if new_prices[i] < 30:
-      cuts_under_30.append(hairstyles[i])
+        cuts_under_30.append(hairstyles[i])
 print(cuts_under_30)
