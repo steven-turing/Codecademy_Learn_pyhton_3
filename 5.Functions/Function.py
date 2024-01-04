0e21652089146771d70216f35d735381c5d92d4b
sijw <1003909016@qq.com>
2024-01-04 18:21:51 +0800

    2024/1/4 commit

--- 5.Functions/Function.py
+++ 5.Functions/Function.py
@@ -65,3 +65,14 @@ def calculate_expenses(plane_ticket_price, car_rental_rate, hotel_rate, trip_tim
 calculate_expenses(200, 100, 100, 5)
 
 # 8.Types of Arguments
+def trip_planner(first_destination,second_destination,final_destination = "Codecademy HQ"):
+  print("Here is what your trip will look like!")
+  print(f"First, we will stop in {first_destination}, then {second_destination}, and lastly {final_destination}")
+
+trip_planner("France", "Germany", "Denmark")
+
+trip_planner("Denmark", "France", "Germany")
+
+trip_planner(first_destination = "Iceland", final_destination = "Germany", second_destination = "India")
+
+trip_planner("Brooklyn",  "Queens")
