711bc7fcee1847092e11c9c2168a20b436518e2b
sijw <1003909016@qq.com>
2024-01-04 14:28:11 +0800

    2024/1/4 commit

---
+++ 5.Functions/Function.py
@@ -0,0 +1,8 @@
+# 1.Introduction to Functions
+# Functions are a convenient way to group our code into reusable blocks. A function contains a sequence of steps that
+# can be performed repeatedly throughout a program without having to repeat the process of writing the same code again.
+
+# 2.Why Functions?
+
+# 3.Defining a Function
+# The def keyword indicates the beginning of a function (also known as a function header).
