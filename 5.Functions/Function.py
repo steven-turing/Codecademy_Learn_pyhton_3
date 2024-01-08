d0be77139ae1282667a500e4e87490f9020f2274
sijw <1003909016@qq.com>
2024-01-08 18:28:12 +0800

    2024/1/8 commit

--- 5.Functions/Function.py
+++ 5.Functions/Function.py
@@ -163,18 +163,23 @@ print(most_popular3)
 def trip_planner_welcome(name):
     print(f"Welcome to tripplanner v1.0 {name} ")
 
+
 trip_planner_welcome("Steven")
+
 
 def estimated_time_rounded(estimated_time):
     rounded_time = round(estimated_time)
     return rounded_time
 
+
 estimate = estimated_time_rounded(50.55)
+
 
 def destination_setup(origin, destination, estimated_time, mode_of_transport="Car"):
     print(f"Your trip starts off in {origin} ")
     print(f"And you are traveling to {destination} ")
     print(f"You will be traveling by {mode_of_transport}")
     print(f"It will take approximately {estimated_time} hours")
+
 
 destination_setup("China", "London", estimate)
