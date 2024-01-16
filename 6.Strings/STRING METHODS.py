0fede17dcd2ef92f6e36b3d10bcb311d1cd90230
sijw <1003909016@qq.com>
2024-01-16 17:50:29 +0800

    2024/1/16 commit

---
+++ 6.Strings/STRING METHODS.py
@@ -0,0 +1,9 @@
+# 1.Introduction
+# Python comes with built-in string methods that give you the power to perform complicated tasks on
+# strings very quickly and efficiently. These string methods allow you to change the case of a string, split a string
+# into many smaller strings, join many small strings together into a larger string, and allow you to neatly combine
+# changing variables with string outputs.
+
+# String methods all have the same syntax:string_name.string_method(arguments)
+
+# 2.Formatting Methods
