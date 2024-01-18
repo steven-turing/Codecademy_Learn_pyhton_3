af0679f234f8ba26e5d19e041563c6741ee03826
sijw <1003909016@qq.com>
2024-01-18 12:02:45 +0800

    2024/1/18 commit

--- 5.Functions/Function.py
+++ 5.Functions/Function.py
@@ -3,7 +3,12 @@
 # can be performed repeatedly throughout a program without having to repeat the process of writing the same code again.
 
 # 2.Why Functions?
+# If our program now had 100 new people trying to find the best directions，
+# we would need to run 100 times!Now, if you’re thinking about using a loop here, your intuition would be totally right!
+# Unfortunately, we won’t be always traveling between the same two locations which means a loop won’t be as effective
+# when we want to customize a trip. We will address this in the upcoming sections!
 
+#For now, let’s gain an appreciation for functions.
 # 3.Defining a Function
 # The def keyword indicates the beginning of a function (also known as a function header).
 
@@ -28,6 +33,8 @@ def directions_to_timesSq():
 directions_to_timesSq()
 
 # 5.Whitespace & Execution Flow
+# In Python, the amount of whitespace tells the computer what is part of a function and what is not part of that
+# function.
 print("Checking the weather for you!")
 
 
@@ -79,17 +86,40 @@ trip_planner(first_destination="Iceland", final_destination="Germany", second_de
 
 trip_planner("Brooklyn", "Queens")
 
-# 9.Built-in Functions vs User Defined Functions
+# 9.Built-in Functions vs User Defined Functions There are two distinct categories for functions in the world of
+# Python. What we have been writing so far in our exercises are called User Defined Functions - functions that are
+# written by users (like us!). help("string")
 
-help("string")
+# There is another category called Built-in functions - functions that come built into Python for us to use. Remember
+# when we were using print or str? Both of these functions are built into the language for us, which means we have
+# been using built-in functions all along!
 
+# There are lots of different built-in functions that we can use in our programs. Take a look at this example of using
+# len() to get the length of a string:
+
+destination_name = "Venkatanarasimharajuvaripeta"
+
+# Built-in function: len()
+length_of_destination = len(destination_name)
+
+# Built-in function: print()
+print(length_of_destination)
+
+# There are even more obscure ones like help() where Python will print a link to documentation for us and provide some
+# details:
+#help("string")
+# Would output (shortened for readability):
+
+# Let’s practice using a few of them. You will need to rely on the provided Python documentation links to find your
+# answers!
+# We were provided a list of prices for some gift shop items:
 tshirt_price = 9.75
 shorts_price = 15.50
 mug_price = 5.99
 poster_price = 2.00
 
-
-# Write your code below:
+# Create a variable called max_price and call the built-in function max() with the variables of prices to get the
+# maximum price.
 def max_price():
     max_price = max(9.75, 15.50, 5.99, 2.00)
     print(max_price)
@@ -97,9 +127,13 @@ def max_price():
 
 max_price()
 
+# Using the same set of prices, create a new variable called min_price and use the built-in function min() with the
+# variables of prices to get the minimum price.
 min_price = min(9.75, 15.50, 5.99, 2.00)
 print(min_price)
 
+
+# Use the built-in function round() to round the price of the variable tshirt_price by one decimal place.
 rounded_price = round(tshirt_price, 1)
 print(rounded_price)
 
@@ -145,6 +179,8 @@ print_remaining_budget(new_budget_after_shirt)
 
 
 # 12.Multiple Returns
+# Sometimes we may want to return more than one value from a function. We can return several values by separating them
+# with a comma.
 def top_tourist_locations_italy():
     first = "Rome"
     second = "Venice"
--- 6.Strings/STRING METHODS.py
+++ 6.Strings/STRING METHODS.py
@@ -7,3 +7,7 @@
 # String methods all have the same syntax:string_name.string_method(arguments)
 
 # 2.Formatting Methods
+# There are three string methods that can change the casing of a string. These are .lower(), .upper(), and .title().
+#   .lower() returns the string with all lowercase characters.
+#   .upper() returns the string with all uppercase characters.
+#   .title() returns the string in title case, which means the first letter of each word is capitalized.
