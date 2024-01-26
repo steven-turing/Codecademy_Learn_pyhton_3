df976be34d0a9d6fb7881f027c720fcff27789c9
sijw <1003909016@qq.com>
2024-01-26 19:23:49 +0800

    2024/1/26 commit

--- Code Challenges/Functions (Advanced).py
+++ Code Challenges/Functions (Advanced).py
@@ -75,6 +75,7 @@ def lots_of_math(a, b, c, d):
   return ((a+b)*(c-d)) % a
 # Uncomment these function calls to test your lots_of_math function:
 print(lots_of_math(1, 2, 3, 4))
-# should print 3, -1, -3, 0
+# =>should print 3, -1, -3, 0
+
 print(lots_of_math(1, 1, 1, 1))
-# should print 2, 0, 0, 0
+# =>should print 2, 0, 0, 0
