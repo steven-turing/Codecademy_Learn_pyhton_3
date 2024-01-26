76247ebe14c6b620c94df72bd36415e61e692d8e
sijw <1003909016@qq.com>
2024-01-26 19:23:32 +0800

    2024/1/26 commit

--- Code Challenges/Functions (Advanced).py
+++ Code Challenges/Functions (Advanced).py
@@ -63,12 +63,10 @@ print(dog_years("Baby", 0))
 # Create a function named lots_of_math(). This function should have four parameters named a, b, c, and d. The function should print 3 lines and return 1 value.
 #
 # First, print the sum of a and b.
-#
 # Second, print c minus d.
-#
 # Third, print the first number printed, multiplied by the second number printed.
-#
 # Finally, return the third number printed modulo a.
+
 # Write your lots_of_math function here:
 def lots_of_math(a, b, c, d):
   print(a+b)
