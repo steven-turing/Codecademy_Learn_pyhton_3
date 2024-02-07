f399f8bba8890ac90c15f3da75ef282da98fa817
sijw <1003909016@qq.com>
2024-02-07 17:58:04 +0800

    2024/2/7 commit

---
+++ 8.Dictionaries/Dictionaries.py
@@ -0,0 +1,5 @@
+#lesson 1： Creating Dictionaries
+# A dictionary is an unordered set of key: value pairs.
+#
+# It provides us with a way to map pieces of data to each other so that we can quickly find values that are associated
+# with one another.
