3923b8cdaa7ac43e1c91703bd17855925c1baaff
sijw <1003909016@qq.com>
2024-08-12 15:05:30 +0800

    2024/2/22 commit

--- 8.Dictionaries/Dictionaries.py
+++ 8.Dictionaries/Dictionaries.py
@@ -1,5 +1,22 @@
 # lesson 1： Creating Dictionaries
+# 1.Introduction to Python Dictionaries
 # A dictionary is an unordered set of key: value pairs.
 #
 # It provides us with a way to map pieces of data to each other so that we can quickly find values that are associated
 # with one another.
+
+# Notice that:
+#
+# A dictionary begins and ends with curly braces { and }.
+# Each item consists of a key ("avocado toast") and a value (6).
+# Each key: value pair is separated by a comma.
+
+# It’s considered good practice to insert a space ( ) after each comma, but our code will still run without the space.
+
+sensors = {"living room": 21, "kitchen": 23, "bedroom": 20}
+# We’ve just added a sensor to the "pantry", and it reads 22 degrees.Add this pair to the dictionary.
+sensors = {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+print(sensors)
+
+# 2.Make a Dictionary
+
