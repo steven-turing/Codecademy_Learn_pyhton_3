12b4d33011b7b61b802145b501cc2ca50338bf06
sijw <1003909016@qq.com>
2024-08-14 19:42:32 +0800

    2024/2/22 commit

--- 9.Files/files.py
+++ 9.Files/files.py
@@ -79,3 +79,27 @@ with open('books.csv') as books_csv:
     for row in books_reader:
         isbn_list.append(row['ISBN'])
     print(isbn_list)
+
+# 10.Writing a CSV File We import the csv library, and then open a new CSV file in write-mode by passing the 'w'
+# argument to the open() function.
+# Now that we’ve instantiated our CSV file writer, we can start adding lines to the file itself! First we want the
+# headers, so we call .writeheader() on the writer object.
+# We call output_writer.writerow() with the item dictionaries which writes each line to the CSV file.
+
+access_log = [{'time': '08:39:37', 'limit': 844404, 'address': '1.227.124.181'},
+              {'time': '13:13:35', 'limit': 543871, 'address': '198.51.139.193'},
+              {'time': '19:40:45', 'limit': 3021, 'address': '172.1.254.208'},
+              {'time': '18:57:16', 'limit': 67031769, 'address': '172.58.247.219'},
+              {'time': '21:17:13', 'limit': 9083, 'address': '124.144.20.113'},
+              {'time': '23:34:17', 'limit': 65913, 'address': '203.236.149.220'},
+              {'time': '13:58:05', 'limit': 1541474, 'address': '192.52.206.76'},
+              {'time': '10:52:00', 'limit': 11465607, 'address': '104.47.149.93'},
+              {'time': '14:56:12', 'limit': 109, 'address': '192.31.185.7'},
+              {'time': '18:56:35', 'limit': 6207, 'address': '2.228.164.197'}]
+fields = ['time', 'address', 'limit']
+
+with open('logger.csv', 'w') as logger_csv:
+    log_writer = csv.DictWriter(logger_csv, fieldnames=fields)
+    log_writer.writeheader()
+    for line in access_log:
+        log_writer.writerow(line)
