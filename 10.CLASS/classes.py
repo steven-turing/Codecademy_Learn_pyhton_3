b2c66957ba2bfaaf74bd62aa1c2c98a6b5a89ca7
sijw <1003909016@qq.com>
2024-08-20 18:14:34 +0800

    2024/2/22 commit

---
+++ 10.CLASS/classes.py
@@ -0,0 +1,9 @@
+# We can check the type of a Python variable using the type() function.A variable’s type determines what you can do
+# with it and how you can use it.
+print(type(5))
+
+my_dict = {}
+print(type(my_dict))
+
+my_list = []
+print(type(my_list))
