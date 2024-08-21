853b8d4a7391b1d0d0d324832d93a62897041748
sijw <1003909016@qq.com>
2024-08-21 13:21:27 +0800

    2024/2/22 commit

--- 10.CLASS/classes.py
+++ 10.CLASS/classes.py
@@ -1,5 +1,6 @@
-# We can check the type of a Python variable using the type() function.A variable’s type determines what you can do
-# with it and how you can use it.
+# 1.Types
+# We can check the type of a Python variable using the type() function.A variable’s type determines what you can
+# do with it and how you can use it.
 print(type(5))
 
 my_dict = {}
@@ -7,3 +8,33 @@ print(type(my_dict))
 
 my_list = []
 print(type(my_list))
+
+
+# 2.Class
+# Define a class using the class keyword.
+class Facade:
+    pass
+
+
+# 3.Instantiation
+# A class doesn’t accomplish anything simply by being defined. A class must be instantiated. In other words, we must
+# create an instance of the class, in order to breathe life into the schematic.
+class Facade:
+    pass
+
+
+facade_1 = Facade()
+
+
+# 4.Object-Oriented Programming
+# A class instance is also called an object. The pattern of defining classes and creating objects to represent the
+# responsibilities of a program is known as Object Oriented Programming or OOP.
+# Instantiation takes a class and turns it into an object, the type() function does the opposite of that.
+class Facade:
+    pass
+
+
+facade_1 = Facade()
+
+facade_1_type = type(facade_1)
+print(facade_1_type)
