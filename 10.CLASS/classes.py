561378775c5533b9de755a030cba688f3889bd44
sijw <1003909016@qq.com>
2024-08-21 13:26:49 +0800

    2024/2/22 commit

--- 10.CLASS/classes.py
+++ 10.CLASS/classes.py
@@ -38,3 +38,15 @@ facade_1 = Facade()
 
 facade_1_type = type(facade_1)
 print(facade_1_type)
+
+
+# 5.Class Variables
+# When we want the same data to be available to every instance of a class we use a class variable. A class variable is a
+# variable that’s the same for every instance of the class.
+#
+# You can define a class variable by including it in the indented part of your class definition, and you can access all
+# of an object’s class variables with object.variable syntax.
+class Grade:
+    minimum_passing = 65
+
+# 6.
