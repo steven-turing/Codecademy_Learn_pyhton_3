fd161580d07a037d83a90be33e8c9006cc619f8a
sijw <1003909016@qq.com>
2024-09-04 20:26:21 +0800

    2024/2/22 commit

--- Code Challenges/Strings.py
+++ Code Challenges/Strings.py
@@ -25,3 +25,20 @@ print(unique_english_letters("mississippi"))
 # should print 4
 print(unique_english_letters("Apple"))
 # should print 4
+
+
+# This is how we solved it:
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+def unique_english_letters(word):
+  uniques = 0
+  for letter in letters:
+    if letter in word:
+      uniques += 1
+  return uniques
+
+# Uncomment these function calls to test your function:
+print(unique_english_letters("mississippi"))
+# should print 4
+print(unique_english_letters("Apple"))
+# should print 4
