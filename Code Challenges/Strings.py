d4a51b405ad49db0ee467c6b8dc86ee9a74aff9b
司锦文 <1003909016@qq.com>
2024-09-05 23:19:12 +0800

    Update Strings.py

--- Code Challenges/Strings.py
+++ Code Challenges/Strings.py
@@ -41,3 +41,27 @@ print(count_char_x("mississippi", "s"))
 # should print 4
 print(count_char_x("mississippi", "m"))
 # should print 1
+
+# Write a function named count_multi_char_x that takes a string named word and a string named x. 
+# This function should do the same thing as the count_char_x function you just wrote - it should return the number of times x appears in word. 
+# However, this time, make sure your function works when x is multiple characters long.
+
+#For example, count_multi_char_x("Mississippi", "iss") should return 2
+# Write your count_multi_char_x function here:
+def count_multi_char_x (word, x):
+   splited_word = word.split(x)
+   count = 0
+   for num in splited_word:
+      count += 1
+   return count - 1
+  
+# Uncomment these function calls to test your function:
+print(count_multi_char_x("mississippi", "iss"))
+# should print 2
+print(count_multi_char_x("apple", "pp"))
+# should print 1
+
+#Here is one possible way to get the answer:
+def count_multi_char_x(word, x):
+  splits = word.split(x)
+  return(len(splits)-1)
