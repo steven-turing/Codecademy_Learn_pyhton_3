d5ca740c59603f1c06226108402cf532fc244804
司锦文 <1003909016@qq.com>
2024-09-05 23:05:21 +0800

    Update Strings.py

--- Code Challenges/Strings.py
+++ Code Challenges/Strings.py
@@ -25,3 +25,19 @@ print(unique_english_letters("mississippi"))
 # should print 4
 print(unique_english_letters("Apple"))
 # should print 4
+
+# Write a function named count_char_x that takes a string named word and a single character named x as parameters. 
+# The function should return the number of times x appears in word.
+
+# Write your count_char_x function here:
+def  count_char_x(word, x):
+  count = 0
+  for a in word:
+    if a == x:
+      count += 1 
+  return count
+# Uncomment these function calls to test your tip function:
+print(count_char_x("mississippi", "s"))
+# should print 4
+print(count_char_x("mississippi", "m"))
+# should print 1
