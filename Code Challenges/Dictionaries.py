df2e2962675b9cb32ca834046c51c3fc22acb034
司锦文 <1003909016@qq.com>
2024-09-18 23:36:02 +0800

    Update Dictionaries.py

--- Code Challenges/Dictionaries.py
+++ Code Challenges/Dictionaries.py
@@ -1,1 +1,15 @@
+# 1. Sum Values
+# Write a function named sum_values that takes a dictionary named my_dictionary as a parameter. The function should return the sum of the values of the dictionary
+
+# Write your sum_values function here:
+def sum_values(my_dictionary):
+  sum = 0
+  for value in my_dictionary.values():
+    sum += value
+  return sum
+# Uncomment these function calls to test your sum_values function:
+print(sum_values({"milk":5, "eggs":2, "flour": 3}))
+# should print 10
+print(sum_values({10:1, 100:2, 1000:3}))
+# should print 6
 
