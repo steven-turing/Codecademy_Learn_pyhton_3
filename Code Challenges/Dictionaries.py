bd6110097d2a1d5cac17fdff8c72efa70761a81b
司锦文 <1003909016@qq.com>
2024-09-18 23:52:28 +0800

    Update Dictionaries.py

--- Code Challenges/Dictionaries.py
+++ Code Challenges/Dictionaries.py
@@ -62,3 +62,11 @@ print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
 # should print [1, 4]
 print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
 # should print ["a"]
+
+#Here is this solution:
+def values_that_are_keys(my_dictionary):
+  value_keys = []
+  for value in my_dictionary.values():
+    if value in my_dictionary:
+      value_keys.append(value)
+  return value_keys
