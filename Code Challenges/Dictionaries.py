86852578639905f0aa1cd831d6988950c2584d8a
司锦文 <1003909016@qq.com>
2024-09-18 23:52:06 +0800

    Update Dictionaries.py

--- Code Challenges/Dictionaries.py
+++ Code Challenges/Dictionaries.py
@@ -45,3 +45,20 @@ print(add_ten({1:5, 2:2, 3:3}))
 print(add_ten({10:1, 100:2, 1000:3}))
 # should print {10:11, 100:12, 1000:13}
 
+# 4. Values That Are Keys
+# Create a function named values_that_are_keys that takes a dictionary named my_dictionary as a parameter. 
+# This function should return a list of all values in the dictionary that are also keys.
+
+# Write your values_that_are_keys function here:
+def values_that_are_keys(my_dictionary):
+  list = []
+  for key in my_dictionary.keys():
+    for value in my_dictionary.values():
+      if key == value:
+        list.append(key)
+  return list
+# Uncomment these function calls to test your  function:
+print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+# should print [1, 4]
+print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+# should print ["a"]
