c41300bc688bc980720d5c0c4296d4ae4b415ba9
司锦文 <1003909016@qq.com>
2024-09-19 22:19:24 +0800

    Update Dictionaries(advanced).py

--- Code Challenges/Dictionaries(advanced).py
+++ Code Challenges/Dictionaries(advanced).py
@@ -1,1 +1,14 @@
 # 1. Word Length Dict
+# Write a function named word_length_dictionary that takes a list of strings named words as a parameter. 
+# The function should return a dictionary of key/value pairs where every key is a word in words and every value is the length of that word.
+# Write your word_length_dictionary function here:
+def word_length_dictionary(words): 
+  dict = {}
+  for word in words:
+    dict[word] = len(word)
+  return dict
+# Uncomment these function calls to test your  function:
+print(word_length_dictionary(["apple", "dog", "cat"]))
+# should print {"apple":5, "dog": 3, "cat":3}
+print(word_length_dictionary(["a", ""]))
+# should print {"a": 1, "": 0}
