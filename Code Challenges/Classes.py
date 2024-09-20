2351c115061e003564d1e1437219f8cd9d94a8c5
司锦文 <1003909016@qq.com>
2024-09-20 23:47:26 +0800

    Update Classes.py

--- Code Challenges/Classes.py
+++ Code Challenges/Classes.py
@@ -18,3 +18,30 @@ robot_1.sensor_range = 10
 print(robot_1.motor_speed)
 print(robot_1.direction)
 print(robot_1.sensor_range)
+
+# 2. Adding Robot Logic
+# In the DriveBot class, add a method called control_bot which accepts parameters: new_speed and new_direction. These should replace the associated instance variables.
+# Create another method called adjust_sensor which accepts a parameter called new_sensor_range which replaces the sensor_range instance variable. 
+# Afterwards, use these methods to rotate the robot 180 degrees at 10 miles per hour with a sensor range of 20 feet. Use the provided print statements to check your code!
+class DriveBot:
+    def __init__(self):
+        self.motor_speed = 0
+        self.direction = 0
+        self.sensor_range = 0
+    
+    # Add the methods here!
+    def control_bot(self,new_speed, new_direction):
+        self.motor_speed = new_speed
+        self.direction = new_direction
+
+    def adjust_sensor(self,new_sensor_range):
+        self.sensor_range = new_sensor_range
+
+robot_1 = DriveBot()
+# Use the methods here!
+robot_1.control_bot(10,180)
+robot_1.adjust_sensor(20)
+
+print(robot_1.motor_speed)
+print(robot_1.direction)
+print(robot_1.sensor_range)
