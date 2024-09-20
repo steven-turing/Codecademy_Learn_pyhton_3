cbe0fe6396d68de18b0af6193843bf3ba3ab5306
司锦文 <1003909016@qq.com>
2024-09-20 23:32:57 +0800

    Update Classes.py

--- Code Challenges/Classes.py
+++ Code Challenges/Classes.py
@@ -1,1 +1,20 @@
+# 1. Setting Up Our Robot
+# Create a python class called DriveBot. Within this class, create instance variables for motor_speed, sensor_range, and direction. All of these should be initialized to 0 by default. 
+# After setting up the class, create an object from the class called robot_1. Set the motor_speed to 5, the direction to 90, and the sensor_range to 10. 
+# Use the provided print statements to check your work!
 
+# Define the DriveBot class here!
+class DriveBot:
+    def __init__(self):
+      self.motor_speed = 0
+      self.direction  = 0
+      self.sensor_range  = 0
+
+robot_1 = DriveBot()
+robot_1.motor_speed = 5
+robot_1.direction = 90
+robot_1.sensor_range = 10
+
+print(robot_1.motor_speed)
+print(robot_1.direction)
+print(robot_1.sensor_range)
